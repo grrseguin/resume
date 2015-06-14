5dafb8c825de26598771c80ac2405258aa116d18
Guillaume Seguin <guillaume.seguin@reseau-canope.fr>
2015-06-14 21:10:51 +0200

    Initialization Yeoman angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cvApp
+ * @description
+ * # cvApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cvApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cvApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cvApp
+ */
+angular.module('cvApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
