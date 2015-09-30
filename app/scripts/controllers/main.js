2e625c8b1f49c9cb6ec75f883e7dabaace6ce062
Guillaume Seguin <guillaume.seguin@reseau-canope.fr>
2015-09-30 23:24:12 +0200

    in progress

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,26 @@
  * Controller of the cvApp
  */
 angular.module('cvApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function(){
+    this.technicalSkills = [
+      {
+        class: 'gray',
+        mdRowspan: '3',
+        mdColspan: '2',
+        mdColspanSm: '1',
+        head: 'Javascript',
+        list: [
+          {
+            name: 'AngularJS'
+          },
+          {
+            name: 'lodash'
+          },
+          {
+            name: 'D3JS'
+          }
+        ],
+        foot: 'C\'est la technologie sur laquelle j\'ai fait le plus de progrès dernièrement.'
+      }
     ];
   });
