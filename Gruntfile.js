ca877dcf7c8df99dc9682ec14c45b48e72b86c9d
Guillaume Seguin <guillaume.seguin@reseau-canope.fr>
2016-01-14 22:59:18 +0100

    fix (Grunt) hostname

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '192.168.0.14',
+        hostname: 'localhost',
         livereload: 35729
       },
       livereload: {
