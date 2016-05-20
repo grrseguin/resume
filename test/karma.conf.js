1802a53ba2bbd30657febbc99ba1decf2daa4465
Guillaume Seguin <guillaume.seguin@reseau-canope.fr>
2016-05-20 08:48:48 +0200

    chore (bower & grunt) rm ref to angular-messages

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,7 +25,6 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-aria/angular-aria.js',
-      'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
